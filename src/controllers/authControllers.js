5e29c2278fb2b4a2c567ffdc984354a3190c82aa
Sunilbasnet-max <sunilbasnet4893@gmail.com>
2025-05-16 18:57:58 +0545

    feat:send token

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -34,7 +34,7 @@ const register = async (req, res) => {
         const formatterData = formatterUserData(user);
         const token = createJwt(formatterData);
         res.cookie("authToken",token);
-        res.json(formatterData);
+        res.json({...formatterData,token:token});
 
     } catch (error) {
         res.status(500).send(error.message);
